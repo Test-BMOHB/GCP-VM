bb889caed008ccddee9d0cbe755cd03962d839ce
ticketcitymr <ticketcitymr@yahoo.com>
2015-10-08 05:54:53 -0500

    Update python_faker.py
    added loop and working faker

--- python_faker.py
+++ python_faker.py
@@ -1,9 +1,9 @@
-#from faker import Faker
-import faker
+from faker import Faker
 import csv
+fake = Faker()
 with open('large.csv','w') as f1:
     writer=csv.writer(f1, delimiter=',',lineterminator='\n',)
-    writer.writerow([''] + range(10))
-    for i in range(10):
-        row = [i] + [10] + fake.name()
+    writer.writerow([''] + range(100))
+    for i in range(100):
+        row = [i] + [10] + [fake.name()] +[fake.address()]
         writer.writerow(row)
