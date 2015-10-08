a00c2c9f33e81a7ae35e743d0a2def32f5cbc64e
ticketcitymr <ticketcitymr@yahoo.com>
2015-10-08 14:34:23 -0500

    Update fakecust.py
    increased to 50MM

--- fakecust.py
+++ fakecust.py
@@ -4,7 +4,7 @@ import csv
 with open('large.csv','w') as f1:
     writer=csv.writer(f1, delimiter=',',lineterminator='\n',)
     writer.writerow([''] + range(10))
-    for i in range(100000):
+    for i in range(50000000):
         row = [i] + [10] + [gen_data.cc_number()]+[gen_data.create_company_name()] +[gen_data.create_email()]+[gen_data.create_name()] +[gen_data.create_job_title()] + [gen_data.create_city_state_zip()] + [gen_data.create_birthday(min_age=2, max_age=85)]
         writer.writerow(row)
 		
