e17367c3335c0825d59a0e4db5d8899e907b2845
Test-BMOHB <michael.rains@bmo.com>
2015-10-24 06:04:24 -0500

    Update random_rdc_trans.py

--- random_rdc_trans.py
+++ random_rdc_trans.py
@@ -3,7 +3,7 @@ import csv
 with open('large_rdc.csv','w') as f1:
     writer=csv.writer(f1, delimiter=',',lineterminator='\n',)
     writer.writerow(['IP'] + range(10))
-    for i in range(100000000):
+    for i in range(1000000):
         countrycode = randrange(0,277,1)
         customerid = randrange(0,999999,1)
         amount = randrange(0,10000,1)
