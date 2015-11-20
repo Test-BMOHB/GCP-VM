ec10cc293f4937b27eb864130ef960c54188a56f
Test-BMOHB <michael.rains@bmo.com>
2015-11-20 14:36:44 -0600

    Update S_P500_algo2.py

--- S_P500_algo2.py
+++ S_P500_algo2.py
@@ -3,7 +3,8 @@
 import urllib2
 import pytz
 import pandas as pd
-
+import numpy as np
+import h5py
 from bs4 import BeautifulSoup
 from datetime import datetime
 from pandas.io.data import DataReader
