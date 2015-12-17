d82c7c7c7538943324f5c735718a84c76a0ada21
JSuelfl <Justin.Suelflow@bmo.com>
2015-12-17 13:06:33 -0600

    Update pyScrap.py

--- pyScrap.py
+++ pyScrap.py
@@ -2,32 +2,40 @@ from lxml import html
 from lxml.etree import tostring
 from datetime import datetime
 import requests, csv, re, time
-
-def scrapPhoneNumbers(mainURL, xPath):
+##  Function to scrap info from toronto.backpage.com
+def scrapInfo(mainURL, xPath):
     mainLinksXPath = mainContent.xpath(xPath)
-    count = 0
     for mainLinksElements in mainLinksXPath:
         link = tostring(mainLinksElements)
+##  Takes out <a href="
         link = link[9:]
         link = link.split()
         link = link[0]
         link = link.split('">')
+##  Only grab the link and not the rest of the HTML 'a' tag
         link = link[0]
-        if re.search("https://my.backpage.com/classifieds", link) is not None:
+##  Moves past pages that have are not postings
+        url = mainURL[:-1]
+        if re.search(url, link) is None:
+            link = url + link
+        if re.search("https://my.backpage.com/classifieds", link) is not None or re.search("http://toronto.backpage.com/adult/?page=", link) is not None or re.search("http://my.backpage.com/classifieds", link) is not None or re.search(mainURL, link) is None:
             continue
-        if re.search(mainURL, link) is None:
-            continue
-        if "http://toronto.backpage.com" not in link:
-            link = "http://toronto.backpage.com" + link
-        count = count + 1
+        phoneNumber = ''
+        emailAddress = ''
+        websiteLink = ''
         linkRequest = requests.get(link)
         linkContent = html.fromstring(linkRequest.content)
+##  Only grabs HTML tags with class = "postingBody"
         linkXPath = linkContent.xpath('//*[@class="postingBody"]')
         for linkXElement in linkXPath:
             text = tostring(linkXElement)
-            phonenumbers = re.findall(r'\d{3}\s\d{3}\s\d{4}|\d{3}\S\d{3}\S\d{4}|\d{3}\d{3}\d{4}|\d{3}\S\s\d{3}\S\d{4}|\d{3}\s\d{7}',text)
+##  Finds phone numbers in the text within the HTML
+##  10 digit regex
+            phonenumbers = re.findall(r'\d{3}\s\d{3}\s\d{4}|\d{3}[^;]\d{3}[^;]\d{4}|\d{3}[^;]\s\d{3}[^;]\d{4}|\d{3}\s\d{7}|\d{3}[^;]\d{7}|\d{10}',text)
+##  If phonenumbers list is blank
             if not phonenumbers:
-                phoneWriter.writerow([link])
+##  11 digit regex
+                phonenumbers = re.findall(r'1\d{3}\s\d{3}\s\d{4}|1\d{3}[^;]\d{3}[^;]\d{4}|1\d{3}[^;]\s\d{3}[^;]\d{4}|1\d{3}\s\d{7}|1\d{3}[^;]\d{7}|1\d{10}',text)
             else:
                 for number in phonenumbers:
                     number = number.replace(' ','')
@@ -36,14 +44,10 @@ def scrapPhoneNumbers(mainURL, xPath):
                     number = number.replace('-','')
                     number = number.replace('.','')
                     number = number.replace('*','')
-                    if number[:1] == 1:
-                        phoneWriter.writerow([link])
-                        otherTextWriter.writerow([text])
-                    elif len(number) == 10:
-                        phoneWriter.writerow([number])
-                    else:
-                        phoneWriter.writerow([link])
-                        otherTextWriter.writerow([text])
+                    if len(number) == 10 or len(number) == 11:
+                        phoneNumber = number
+##                      phoneWriter.writerow([number])
+##  Only grabs HTML 'a' tags in the tags that have class = "postingBody"
         linkXPath = linkContent.xpath('//*[@class="postingBody"]/a')
         for linkXElement in linkXPath:
             textA = tostring(linkXElement)
@@ -52,62 +56,77 @@ def scrapPhoneNumbers(mainURL, xPath):
                 end = textA.find('">')
                 if start != -1 and end != -1:
                     textA = textA[start+8:end]
-                    emailWriter.writerow([textA])
+                    emailAddress = textA
+##                    emailWriter.writerow([textA])
+            if 'http' in textA:
+                start = textA.find('http')
+                end = textA.find('">')
+                if start != -1 and end != -1:
+                    textA = textA[start:end]
+                    websiteLink = textA
+##                    webWriter.writerow([textA])
+##  Adds list item
+        li.append([phoneNumber,emailAddress,websiteLink,link])
 
-with open('PhoneNumbers.csv','w') as phoneFile:
-    with open('Emails.csv','w') as emailFile:
-        with open('Text.csv','w') as otherTextFile:
-            startTotTime = time.time()
-            mainURL = "http://toronto.backpage.com/"
-            currDate = datetime.now().strftime('%Y-%m-%d')
-            phoneWriter=csv.writer(phoneFile)
-            emailWriter=csv.writer(emailFile)
-            otherTextWriter=csv.writer(otherTextFile)
-            increment = 1
-            while increment < 1000:
-                print "Page: " + str(increment)
-                if increment == 1:
-                    mainRequest = requests.get(mainURL + "adult/")
-                else:
-                    mainRequest = requests.get(mainURL + "adult/?page=" + str(increment))
-                mainContent = html.fromstring(mainRequest.content)
-##	Current date
-                date = mainContent.xpath('//*[@class="date"]')
-                for dateStr in date:
-                    dateStr = tostring(dateStr)
-                    dateStr = re.search("\w{3}. \w{3}. \d{2}", dateStr)
-                    dateStr = datetime.strptime(dateStr.group(), '%a. %b. %d').date()
-                    dateStr = dateStr.replace(year=datetime.now().year)
-                    dateStr = dateStr.strftime('%Y-%m-%d')
-                if dateStr == currDate:
-##	Start of Main Links
-                    scrapPhoneNumbers(mainURL, '/html/body/div//*[@href]')
-##	End of Main Links
-##	Start of Sponsors Links
-                    scrapPhoneNumbers(mainURL, '//*[@class="sponsorBoxContent"]/a')
-##	End Sponsor Links
-                else:
-                    endTotTime = time.time()
-                    totTime = endTotTime - startTotTime
-                    totTime = ("{0:.1f}".format(round(totTime,2)))
-                    print "It took " + str(totTime) + " seconds to complete today's pages."
-                    exit()
-                increment = increment + 1
+##  Function to remove duplicate list entries
+def removeDuplicates(li):
+    count1 = 0
+    for i in li:
+        count2 = 0
+        for e in li:
+##  Don't compare the same exact list index
+            if count1 == count2:
+                continue
+            else:
+                if i == e:
+                    li.remove(e)
+## If all information is the same except for the backpage link, delete the list entry
+                elif i[0] == e[0] and i[1] == e[1] and i[2] == e[2] and i[3] != e[3]:
+                    li.remove(e)
+            count2 = count2 + 1
+        count1 = count1 + 1
 
+##  Function to create the CSV file
+def createCSV(li):
+    with open('ScreenScrap.csv','w') as phoneFile:
+        phoneWriter = csv.writer(phoneFile, delimiter=',')
+##  Add a header row
+        phoneWriter.writerow(["PhoneNumber","Email Address", "Website", "BackPage Link"])
+        for i in li:
+            phoneWriter.writerow(i)
 
-
-
-
-
-##        x3 = c2.xpath('//*[@id="postingTitle"]')
-##        for e2 in x2:
-##            strE2 = tostring(e2)
-##            writer.writerow(strE2)
-##        for e3 in x3:
-##            strE3 = tostring(e3)
-##            writer.writerow(strE3)
-
-##gsutil cp gs://web_crawler_bmo/PhoneNumbers.csv .
-##gsutil cp PhoneNumbers.csv gs://web_crawler_bmo/
-##gsutil cp Emails.csv gs://web_crawler_bmo/
-##gsutil cp Text.csv gs://web_crawler_bmo/
+startTime = time.time()
+mainURL = "http://toronto.backpage.com/"
+currDate = datetime.now().strftime('%Y-%m-%d')
+li = []
+increment = 1
+##  Increment through 999 possible pages
+while increment < 1000:
+    print "Page: " + str(increment)
+    if increment == 1:
+        mainRequest = requests.get(mainURL + "adult/")
+    else:
+##  To only print one page, comment out the mainRequest line and uncomment the break line                    
+##        break
+        mainRequest = requests.get(mainURL + "adult/?page=" + str(increment))
+    mainContent = html.fromstring(mainRequest.content)
+    date = mainContent.xpath('//*[@class="date"]')
+    for dateStr in date:
+        dateStr = tostring(dateStr)
+        dateStr = re.search("\w{3}. \w{3}. \d{2}", dateStr)
+        dateStr = datetime.strptime(dateStr.group(), '%a. %b. %d').date()
+        dateStr = dateStr.replace(year=datetime.now().year)
+        dateStr = dateStr.strftime('%Y-%m-%d')
+##  Compare current date to date on webpage
+    if dateStr == currDate:
+        scrapInfo(mainURL, '/html/body/div//*[@href]')
+        scrapInfo(mainURL, '//*[@class="sponsorBoxContent"]/a')
+        removeDuplicates(li)
+        createCSV(li)
+    else:
+        endTime = time.time()
+        totTime = endTime - startTime
+        totTime = ("{0:.1f}".format(round(totTime,2)))
+        print "It took " + str(totTime) + " seconds to complete today's pages."
+        exit()
+    increment = increment + 1
