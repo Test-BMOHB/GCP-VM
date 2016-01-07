2785d4344bc5e722843e07d9b2b073d2b6f20b46
JSuelfl <Justin.Suelflow@bmo.com>
2016-01-07 11:21:03 -0600

    Update pyScrap.py

--- pyScrap.py
+++ pyScrap.py
@@ -1,11 +1,10 @@
 from lxml import html
 from lxml.etree import tostring
-from datetime import datetime
+from datetime import datetime, timedelta
 import requests, csv, re, time
-liHTML = []
-##  Function to scrap info from toronto.backpage.com
-def scrapInfo(mainURL, mainContent, xPath):
-    global liHTML
+##  Function to scrape info
+def scrapeInfo(mainURL, mainContent, xPath):
+##    global liHTML
     li = []
     mainLinksXPath = mainContent.xpath(xPath)
     for mainLinksElements in mainLinksXPath:
@@ -33,7 +32,6 @@ def scrapInfo(mainURL, mainContent, xPath):
         for linkXElement in linkXPath:
             phoneNumber = []
             text = tostring(linkXElement)
-            pageHTML = text
 ##  Take out small icons in text
             icons = re.findall(r'&#\d*;', text)
             for icon in icons:
@@ -41,9 +39,6 @@ def scrapInfo(mainURL, mainContent, xPath):
 ##  Take out HTML tags             
             tags = re.findall('<[^>]+>', text)
             for tag in tags:
-##                tag = '<' + tag + '>'
-##                if '(' in tag or ')' in tag:
-##                    print tag
                 text = text.replace(tag, '')
 ##  Take out whitespace
             text = re.sub('\s*', '', text)
@@ -87,11 +82,11 @@ def scrapInfo(mainURL, mainContent, xPath):
             numReplace = re.compile(re.escape('ninety'), re.IGNORECASE)
             text = numReplace.sub('90', text)
 ##  Finds phone numbers in the text within the HTML
-##  10 digit regex
-            phonenumbers = re.findall(r'\d{3}[^a-zA-Z]*\d{3}[^a-zA-Z]*\d{4}|\d[.]*\d[.]*\d[.]*\d[.]*\d[.]*\d[.]*\d[.]*\d[.]*\d[.]*\d|\d[-]*\d[-]*\d[-]*\d[-]*\d[-]*\d[-]*\d[-]*\d[-]*\d[-]*\d',text)
 ##  11 digit regex
+            phonenumbers = re.findall(r'1\d{3}[^a-zA-Z]*\d{3}[^a-zA-Z]*\d{4}|1[.]*\d[.]*\d[.]*\d[.]*\d[.]*\d[.]*\d[.]*\d[.]*\d[.]*\d[.]*\d|1\d[-]*\d[-]*\d[-]*\d[-]*\d[-]*\d[-]*\d[-]*\d[-]*\d[-]*\d|1[-]\d{3}[-]\d{3}[-]\d{4}',text)
+##  10 digit regex
             if not phonenumbers:
-                phonenumbers = re.findall(r'1\d{3}[^a-zA-Z]*\d{3}[^a-zA-Z]*\d{4}|1[.]*\d[.]*\d[.]*\d[.]*\d[.]*\d[.]*\d[.]*\d[.]*\d[.]*\d[.]*\d|1\d[-]*\d[-]*\d[-]*\d[-]*\d[-]*\d[-]*\d[-]*\d[-]*\d[-]*\d',text)
+                phonenumbers = re.findall(r'\d{3}[^a-zA-Z]*\d{3}[^a-zA-Z]*\d{4}|\d[.]*\d[.]*\d[.]*\d[.]*\d[.]*\d[.]*\d[.]*\d[.]*\d[.]*\d|\d[-]*\d[-]*\d[-]*\d[-]*\d[-]*\d[-]*\d[-]*\d[-]*\d[-]*\d|\d{3}[-]\d{3}[-]\d{4}',text)
             if phonenumbers:
                 for number in phonenumbers:
                     number = number.replace(' ','')
@@ -103,10 +98,6 @@ def scrapInfo(mainURL, mainContent, xPath):
                     number = number.replace('~','')
                     if len(number) == 10 or len(number) == 11:
                         phoneNumber.append(number)
-##                      phoneWriter.writerow([number])
-##  If phonenumbers list is blank
-            else:
-                liHTML.append([pageHTML])
 ##  Only grabs HTML 'a' tags in the tags that have class = "postingBody"
         emailAddress = []
         websiteLink = []
@@ -121,75 +112,45 @@ def scrapInfo(mainURL, mainContent, xPath):
                 if start != -1 and end != -1:
                     textA = textA[start+8:end]
                     emailAddress.append(textA)
-##                    emailWriter.writerow([textA])
             if 'http' in textA:
                 start = textA.find('http')
                 end = textA.find('">')
                 if start != -1 and end != -1:
                     textA = textA[start:end]
                     websiteLink.append(textA)
-##                    webWriter.writerow([textA])
 ##  Adds list item
-        li.append([phoneNumber,emailAddress,websiteLink,link])
+        li.append([phoneNumber,emailAddress,websiteLink,[link]])
     return li
-
-##  Function to remove duplicate list entries
-def removeDuplicates(dedup):
-    count1 = 0
-    finalList = []
-    print len(dedup)
-    for i in dedup:
-        count2 = 0
-        for e in dedup:
-##  Don't compare the same exact list index
-            if count1 == count2:
-                continue
-            else:
-## If all information is the same, delete the list entry
-                if i == e:
-                    dedup.pop(count2)
-## If all information is the same except for the backpage link, delete the list entry
-                if i[0] is not None and e[0] is not None:
-                    if i[1] is not None and e[1] is not None:
-                        if i[2] is not None and e[2] is not None:
-                            if i[0] == e[0] and i[1] == e[1] and i[2] == e[2]:
-                                dedup.pop(count2)
-                        else:
-                            if i[0] == e[0] and i[1] == e[1]:
-                                dedup.pop(count2)
-                    elif i[2] is not None and e[2] is not None:
-                        if i[0] == e[0] and i[2] == e[2]:
-                                dedup.pop(count2)
-                    else:
-                        if i[0] == e[0]:
-                                dedup.pop(count2)
-            count2 = count2 + 1
-        count1 = count1 + 1
-    for i in dedup:
-        finalList.append(i)
-    print len(finalList)
-    return finalList
 
 ##  Function to create the CSV file
 def createCSV(liCSV, f1):
     writer = csv.writer(f1, delimiter=',', quoting=csv.QUOTE_NONE, escapechar=' ')
 ##  Add a header row
-    writer.writerow(["PhoneNumber","Email Address", "Website", "BackPage Link"])
+    writer.writerow(["PhoneNumber","Email_Address","Website","BackPage_Link"])
     for i in liCSV:
         rowStr = ''
+        num = []
+        write = True
         for n in i:
             if isinstance(n, list):
-                countE = 0
-                for e in n:
-                    if countE == 0:
-                        rowStr = rowStr + e
-                    else:
-                       rowStr = rowStr + ':' + e
-                    countE = countE + 1
+                if len(n) > 1:
+                    num = n
+                    write = False
+                else:
+                    rowStr = rowStr + ''.join(n)
             else:
                 rowStr = rowStr + n
             rowStr = rowStr + ","
-        writer.writerow([rowStr])
+        rowStr = rowStr[:-1]
+        if write:
+            writer.writerow([rowStr])
+        else:
+            num = list(set(num))
+            rowCopy = rowStr
+            for e in num:
+                li = [e]
+                rowStr = ''.join(li) + rowCopy
+                writer.writerow([rowStr])
 
 ##  Start timer
 def startTimer():
@@ -203,69 +164,71 @@ def endTimer(st):
     tot = ("{0:.1f}".format(round(tot,2)))
     print "It took " + str(tot) + " seconds to complete process."
 
+##  Function to remove exact duplicate list entries
+def removeDuplicates(dedup):
+    print len(dedup)
+    finalList = []
+    for x in dedup:
+        if x not in finalList:
+            finalList.append(x)
+    print len(finalList)
+    return finalList
+
 ##  Main Function
 def main():
-    global liHTML
     startTime = time.time()
     mainURL = "http://toronto.backpage.com/"
-    currDate = datetime.now().strftime('%Y-%m-%d')
+    currDate = datetime.now()
+##  Make currDate Yesterday's date
+    currDate = currDate - timedelta(days=1)
+    currDate = currDate.strftime('%Y-%m-%d')
     liData = []
     increment = 1
 ##  Increment through 999 possible pages
-    while increment < 1000:
-        print "Page: " + str(increment)
-        if increment == 1:
-            mainRequest = requests.get(mainURL + "adult/")
-        else:
-##  To only print one page, change the increment if statement to == 2
-##  To print all page's in a day, comment out the if statement below
-            if increment == 3:
-                currDate = ''
-            mainRequest = requests.get(mainURL + "adult/?page=" + str(increment))
-        mainContent = html.fromstring(mainRequest.content)
-        date = mainContent.xpath('//*[@class="date"]')
-        for dateStr in date:
-            dateStr = tostring(dateStr)
-            dateStr = re.search("\w{3}. \w{3}. \d{2}", dateStr)
-            dateStr = datetime.strptime(dateStr.group(), '%a. %b. %d').date()
-            dateStr = dateStr.replace(year=datetime.now().year)
-            dateStr = dateStr.strftime('%Y-%m-%d')
+    with open('ScreenScrape.csv','w') as scrapeFile:
+        while increment < 1000:
+            print "Page: " + str(increment)
+            if increment == 1:
+                mainRequest = requests.get(mainURL + "adult/")
+            else:
+                mainRequest = requests.get(mainURL + "adult/?page=" + str(increment))
+            mainContent = html.fromstring(mainRequest.content)
+            date = mainContent.xpath('//*[@class="date"]')
+            for dateStr in date:
+                dateStr = tostring(dateStr)
+                dateStr = re.search("\w{3}. \w{3}. \d{1,2}", dateStr)
+                dateStr = datetime.strptime(dateStr.group(), '%a. %b. %d').date()
+                dateStr = dateStr.replace(year=datetime.now().year)
+                dateStr = dateStr.strftime('%Y-%m-%d')
 ##  Compare current date to date on webpage
-        if dateStr == currDate:
-            startT = startTimer()
-            print "Main Scrap"
-            liData.extend(scrapInfo(mainURL, mainContent, '/html/body/div//*[@href]'))
-            endTimer(startT)
+            if dateStr == currDate:
+                startT = startTimer()
+                print "Main Scrape"
+                liData.extend(scrapeInfo(mainURL, mainContent, '/html/body/div//*[@href]'))
+                endTimer(startT)
 ##  Extend liData to include anything from the sponsorBoxContent xPath
-            startT = startTimer()
-            print "Sponsor Scrap"
-            liData.extend(scrapInfo(mainURL, mainContent, '//*[@class="sponsorBoxContent"]/a'))
-            endTimer(startT)
-        else:
-            startT = startTimer()
-            print "Remove Dups from Scrap"
-            liData = removeDuplicates(liData)
-            endTimer(startT)
-            startT = startTimer()
-            print "Remove Dups from HTML"
-            liHTML = removeDuplicates(liHTML)
-            endTimer(startT)
-            with open('ScreenScrap.csv','w') as scrapFile:
                 startT = startTimer()
-                print "Create CSV for Scrap"
-                createCSV(liData, scrapFile)
+                print "Sponsor Scrape"
+                liData.extend(scrapeInfo(mainURL, mainContent, '//*[@class="sponsorBoxContent"]/a'))
                 endTimer(startT)
-            with open('Text.csv','w') as htmlFile:
+            elif currDate < dateStr and currDate <> '':
+                increment = increment + 1
+                continue
+            else:
                 startT = startTimer()
-                print "Create CSV for HTML"
-                createCSV(liHTML, htmlFile)
+                print "Remove Dups from Scrape"
+                liData = removeDuplicates(liData)
                 endTimer(startT)
-            endTime = time.time()
-            totTime = endTime - startTime
-            totTime = ("{0:.1f}".format(round(totTime,2)))
-            print "It took " + str(totTime) + " seconds to complete today's pages."
-            exit()
-        increment = increment + 1
+                startT = startTimer()
+                print "Create CSV for Scrape"
+                createCSV(liData, scrapeFile)
+                endTimer(startT)
+                endTime = time.time()
+                totTime = endTime - startTime
+                totTime = ("{0:.1f}".format(round(totTime,2)))
+                print "It took " + str(totTime) + " seconds to complete today's pages."
+                exit()
+            increment = increment + 1
 
 ##  Run main
 main()
